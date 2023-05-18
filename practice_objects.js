530a5f6b367871f5ee3117e42acb24bb00c1a6c0
shayim520 <xayimardan520@gmail.com>
2023-05-18 12:32:32 -0400

    practice_object

---
+++ practice_objects.js
@@ -0,0 +1,18 @@
+// Arrays : 
+
+let scores = [99, 67, 59, 77, 100];
+console.log(scores);
+// get the first item in array
+console.log(scores[0]);  //99 
+
+// count how many elements in array 
+console.log('Item count: ' + scores.length);
+
+// JavaScript object
+let person1 = {
+    firstName: "Elnara",
+    lastName: "H",
+    age: 18,
+}
+
+console.log(object);
