e5fe7856abde63a357a9018745fcce23724ea9e2
shayim520 <xayimardan520@gmail.com>
2023-05-18 12:04:59 -0400

    JsOperator

--- practice2_operator.js
+++ practice2_operator.js
@@ -1,34 +1,36 @@
-let num = 100;
+let num = 100; 
+// Arithmetic Operators  + - * / %    
+  // ++ --
+console.log(num + 5 );
+console.log(num - 5 );
+console.log(num * 5 );
+console.log(num / 5 );
+console.log(num % 5 );
 
-// Arithmetic operator + - * / %
-    // ++ --
-    console.log(num + 5 );
-    console.log(num - 5 );
-    console.log(num * 5 );
-    console.log(num / 5 );
-    console.log(num % 5 );
+console.log("-----------");
+// Assignments Operators 
+// =   +=  -=  *= /=  %=
+console.log(num += 100);
+console.log(num -= 50);
+console.log(num *= 2);
+console.log(num /= 2);
 
+console.log("-----------");
 
-    console.log("--------------------------");
-    // Assignment Operators
-    //  =  +=  -=  *=  /=  %=
-    console.log(num += 100);
-    console.log(num -= 50);
-    console.log(num *= 2);
-    console.log(num /= 2);
+// Relational|Comparison Operators 
+    // == ===  !=  !==  > >= < <=
+let price = 99; 
+let strPrice = "99";
 
-    console.log("--------------------------");
-    // Relational(Comparison) Operators
-    //  ==   ===  !=  !==  >   >=   <  <=
-    let price = 99;
-    let strPrice = '99';
+console.log(price == strPrice); // true: just check for content equality
+console.log(price === strPrice); //false : check for content and type equality
 
-    console.log(price == strPrice); // this one give us True / just check for content equality
-    console.log(price === strPrice); // this one give us False / check for content and type equality
-    console.log(price !=99); // false
+console.log(price!=99);  //false
 
-    console.log("--------------------------");
+// Logical Operators  && ||   &  |
+// check is the price value between 50 - 100 
+console.log(price < 100 && price > 50); // true
+// check the price is less than 10 or greater than 100
+console.log(price < 10 || price > 100);
 
-// Logical Operators  && ||   & , |
-// check is the price value between 50 - 100
-console.log(price<100 && price>50);
+// Conditional Statement 
