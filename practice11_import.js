1f335952f448f8ca005495223149769507397d79
shayim520 <xayimardan520@gmail.com>
2023-05-22 14:43:55 -0400

    import and export

--- practice11_import.js
+++ practice11_import.js
@@ -1,8 +1,21 @@
 import { sayHelloTo, addNumbers } from "./practice11_methods.js";
 
+import chalk from "chalk";
+
+import Person from "./practice12_person.js";
+
 sayHelloTo("John");
 sayHelloTo("Shay");
+
+
 console.log(addNumbers(52,96));
 
+console.log(chalk.blue("Hello world"));
+
+
+const p1 = new Person("Dua", "D");
+p1.printFullName();
+// p1.age = 17;
+// console.log(p1);
 
 
